a9a7f524195c817039d63f47545735a9c83eb29d
Daniel Njoo <danielnjoo@vlan422-190.subnet-237.amherst.edu>
2017-12-11 03:42:31 -0500

    donezoes :')

--- shiny/server.R
+++ shiny/server.R
@@ -2,7 +2,17 @@ library(shiny)
 library(readxl)
 library(tidyverse)
 library(reshape2)
+library(dbscan)
 data<-readxl::read_xlsx('./../twitter_data.xlsx')
+
+visualization <- data[4:nrow(data),] %>% 
+  sapply(., as.numeric) %>% 
+  as.data.frame()
+names(visualization) <- data[3, ]
+visualization <- visualization %>% select("openness", "conscientiousness", "extraversion", "agreeableness", 
+                                          "Neuroticism", "Machiavellianism", "narcissism", "psychopathy", 
+                                          "followers_count", "statuses_count", "favourites_count", "kloutscore", 
+                                          "PercentOrigTweets", "PercentRT", "PercentReplies", "friends_count")
 
 big5 <- data[4:nrow(data),2:6] %>% sapply(., as.numeric) %>% as.data.frame()
 names(big5) <- data[3, 2:6]
@@ -31,28 +41,105 @@ pcaall8 <- all8 %>%
 
 shinyServer(function(input, output) {
   
-  userTweetData <- eventReactive(input$showUserTweets, {
-    withProgress(message = "Loading application", value = 0, {
-      tweets <- userTimeline(input$twitterUser1, n = 500) #selects specified number of user tweets
-      incProgress(0.7, detail = "Getting tweets")
-      # tab <- twListToDF(tweets) %>% .[!duplicated(tab[,c('text')]),] 
-      
-      tab <- twListToDF(tweets) %>% .[!duplicated(tab[,c('text')]),] %>% dplyr::select(text, favoriteCount, retweetCount, created,
-                                                                                isRetweet, retweeted, longitude, latitude) 
-      
-      
-      # create list of multiple result? and return that? datatable as above... what else?
-      
-      # tab <- twListToDF(tweets) #converts tweets and associated metrics in table format
-      # tab2 <- tab[!duplicated(tab[,c('text')]),] #removes duplicated text
-      
-      # tab2 <- tab2 %>% dplyr::select(text, favoriteCount)
-      # tab2 <- tab2 %>% dplyr::select(text, favoriteCount, replyToSN, created, truncated, replyToSID, id, replyToUID, statusSource, screenName, retweetCount,
-                                     # isRetweet, retweeted, longitude, latitude)
-      # print(tab2)
-      incProgress(0.3, detail = "Finishing...")
-      return(tab)
-    })
+  output$initialExp <- renderPlot({
+    if (input$openness==T){
+      initialExpX <- visualization$openness
+      xLabel <- "Openness"
+    } else if (input$conscientiousness==T){
+      initialExpX <- visualization$conscientiousness
+      xLabel <- "Conscientiousness"
+    } else if (input$extraversion==T){
+      initialExpX <- visualization$extraversion
+      xLabel <- "Extraversion"
+    } else if (input$agreeableness==T){
+      initialExpX <- visualization$agreeableness
+      xLabel <- "Agreeableness"
+    } else if (input$Neuroticism==T){
+      initialExpX <- visualization$Neuroticism
+      xLabel <- "Neuroticism"
+    } else if (input$Machiavellianism==T){
+      initialExpX <- visualization$Machiavellianism
+      xLabel <- "Machiavellianism"
+    } else if (input$narcissism==T){
+      initialExpX <- visualization$narcissism
+      xLabel <- "Narcissism"
+    } else {
+      initialExpX <- visualization$psychopathy
+      xLabel <- "Psychopathy"
+    } 
+    title <- paste0(("Density plot of "), xLabel)
+    visualization %>% ggplot(aes(initialExpX)) + geom_density() + xlab(xLabel) + ggtitle(title)
+  })
+  
+  output$relExp <- renderPlot({
+    if (input$openness2==T){
+      relExpX <- visualization$openness
+      xLabel <- "Openness"
+    } else if (input$conscientiousness2==T){
+      relExpX <- visualization$conscientiousness
+      xLabel <- "Conscientiousness"
+    } else if (input$extraversion2==T){
+      relExpX <- visualization$extraversion
+      xLabel <- "Extraversion"
+    } else if (input$agreeableness2==T){
+      relExpX <- visualization$agreeableness
+      xLabel <- "Agreeableness"
+    } else if (input$Neuroticism2==T){
+      relExpX <- visualization$Neuroticism
+      xLabel <- "Neuroticism"
+    } else if (input$Machiavellianism2==T){
+      relExpX <- visualization$Machiavellianism
+      xLabel <- "Machiavellianism"
+    } else if (input$narcissism2==T){
+      relExpX <- visualization$narcissism
+      xLabel <- "Narcissism"
+    } else {
+      relExpX <- visualization$psychopathy
+      xLabel <- "Psychopathy"
+    } 
+    
+    if (input$followers_count==T){ 
+      max <- which.max(visualization$followers_count)
+      visualization <- visualization[-max,]
+      relExpX <- relExpX[-max]
+      # relExpY <- visualization$followers_count[-max]
+      yLabel <- "Followers Count"
+    } else if (input$statuses_count==T){
+      relExpY <- visualization$statuses_count
+      yLabel <- "Statuses Count"
+    } else if (input$favourites_count==T){
+      relExpY <- visualization$favourites_count
+      yLabel <- "Favourites Count"
+    }  else if (input$friends_count==T){
+      relExpY <- visualization$friends_count
+      yLabel <- "Friends Count" 
+    }  else if (input$kloutscore==T){
+      relExpY <- visualization$kloutscore
+      yLabel <- "Kloutscore"
+    } else if (input$PercentRT==T){
+      relExpY <- visualization$PercentRT
+      yLabel <- "Percent Retweets"
+    } else {
+      relExpY <- visualization$PercentReplies
+      yLabel <- "Percent Replies"
+    }
+    if (input$geom_smooth==F){
+      if (input$log==F){
+        visualization %>% ggplot(aes(relExpX, relExpY)) + geom_point() + geom_jitter() +
+          xlab(xLabel) + ylab(yLabel) + ggtitle(paste0(xLabel, " on ", yLabel))
+      } else {
+        visualization %>% ggplot(aes(relExpX, log(relExpY))) + geom_point() + geom_jitter() +
+          xlab(xLabel) + ylab(paste0(yLabel," (Log)")) + ggtitle(paste0(xLabel, " on ", yLabel," (Log)"))
+      }
+    } else {
+      if (input$log==F){
+        visualization %>% ggplot(aes(relExpX, relExpY)) + geom_point() + geom_jitter() +
+          xlab(xLabel) + ylab(yLabel) + geom_smooth(se=F) + ggtitle(paste0(xLabel, " on ", yLabel))
+      } else {
+        visualization %>% ggplot(aes(relExpX, log(relExpY))) + geom_point() + geom_jitter() +
+          xlab(xLabel) + ylab(paste0(yLabel," (Log)")) + geom_smooth(se=F) + ggtitle(paste0(xLabel, " on ", yLabel," (Log)"))
+      }
+    }
   })
   
   output$clusterPlot <- renderPlot({
@@ -98,16 +185,97 @@ shinyServer(function(input, output) {
         theme(axis.text.x = element_text(angle=90, vjust=0.5,hjust=1))
   })
   
-  temp <- reactive ({
-    temp <- userTweetData()  
-    print(temp)
+  output$catAndTweets <- renderPlot({
+
+    kmeans(big5, input$clusters)$cluster %>% 
+      as.data.frame() %>% 
+      cbind(visualization) -> temp2
+    names(temp2)[1] <- 'cat'
+    
+    if (input$followers_count2==T){ 
+      max <- which.max(temp2$followers_count)
+      temp2 <- temp2[-max,]
+      relExpX <- relExpX[-max]
+      yLabel <- "Followers Count"
+    } else if (input$statuses_count2==T){
+      relExpY <- temp2$statuses_count
+      yLabel <- "Statuses Count"
+    } else if (input$favourites_count2==T){
+      relExpY <- temp2$favourites_count
+      yLabel <- "Favourites Count"
+    } else if (input$friends_count2==T){
+      relExpY <- temp2$friends_count
+      yLabel <- "Friends Count"
+    } else if (input$kloutscore2==T){
+      relExpY <- temp2$kloutscore
+      yLabel <- "Kloutscore"
+    } else if (input$PercentRT2==T){
+      relExpY <- temp2$PercentRT
+      yLabel <- "Percent Retweets"
+    } else {
+      relExpY <- temp2$PercentReplies
+      yLabel <- "Percent Replies"
+    }
+    
+    g <- temp2 %>% ggplot(aes(as.factor(cat), as.numeric(relExpY))) + geom_boxplot(outlier.shape=NA)
+    ylim1 = boxplot.stats(relExpY)$stats[c(1, 5)]
+    g + coord_cartesian(ylim = ylim1) + ylab(yLabel) + xlab("Personality Type According to KNN clusters")
+    
   })
   
+  userTweetData <- eventReactive(input$showUserTweets, {
+    getUser(input$twitterUser1) -> user
+    c(user$favoritesCount, user$followersCount, user$friendsCount, user$statusesCount) %>% as.data.frame() -> temp
+    rownames(temp) <- c("Favourites Count", "Followers Count", "Friends Count", "Statuses Count")
+    return(temp)
+    # withProgress(message = "Loading application", value = 0, {
+    #   tweets <- userTimeline(input$twitterUser1, n = 500) #selects specified number of user tweets
+    #   incProgress(0.7, detail = "Getting tweets")
+    #   tab <- twListToDF(tweets) #converts tweets and associated metrics in table format
+    #   tab2 <- tab[!duplicated(tab[,c('text')]),] #removes duplicated text
+    #   tab2 <- tab2 %>% dplyr::select(text, favoriteCount, retweetCount, created,
+    #                                  isRetweet, retweeted, longitude, latitude)
+    #   incProgress(0.3, detail = "Finishing...")
+    #   return(tab2)
+    # })
+  })
   
   output$userTweets <- renderDataTable({
-    # print(userTweetData())
-    # userTweetData()
-    temp
+    userTweetData()
+  })
+  
+  getPreds <- eventReactive(input$getPreds, {
+    
+    kmeans(big5, input$clusters)$cluster %>% 
+      as.data.frame() %>% 
+      cbind(visualization) -> temp3
+    
+    names(temp3)[1] <- 'cat'
+    
+    temp3 %>% dplyr::select(favourites_count, followers_count, friends_count, statuses_count) %>% 
+      mutate(
+        favourites_count = parse_number(favourites_count),
+        followers_count = parse_number(followers_count),
+        friends_count = parse_number(friends_count),
+        statuses_count = parse_number(statuses_count)
+      ) %>% na.omit -> others
+    
+    userTweetData() -> temp
+    
+    rownames(temp) <- colnames(others)
+    
+    t(temp) %>% rbind(others) %>% data.matrix %>% kNN(x=.,k=100) %>% .$id %>% head(1) -> closest_100_ids
+    
+    prop.table(table(temp3[closest_100_ids,'cat'])) %>% 
+      data.frame %>% 
+      arrange(desc(Freq)) %>% 
+      rename('Personality Type' = Var1 ,Probability=Freq) %>% 
+      return()
+    
+  })
+  
+  output$closestTweets <- renderDataTable({
+   getPreds() 
   })
   
 })
--- shiny/ui.R
+++ shiny/ui.R
@@ -19,23 +19,120 @@ shinyUI(fluidPage(
   
   titlePanel("Twitter Personality Exploration"),
   
+  p("In this Shiny App, we explore one of the datasets that matches personality scores (Big5 and Dark Triad)
+    with online behaviour, in this case Twitter statistics. This dataset was obtained via private communication
+    with one of the organizers of the Kaggle competition found ", tags$a(href="kaggle.com/c/twitter-personality-prediction", 
+                                                                         "here.")),
+  
+  tags$ul(
+    tags$li("We initially explore the personality dimensions of the ~3000 profiles in this dataset"),
+    tags$li("Then explore potential relationships between their Twitter usage via statistics such as the number of 
+    followers they have and their personality scores"),
+    tags$li("Then, we use a KNN-clustering technique to create personality categories for us, which we then use to 
+    again explore relationships with Twitter usage"),
+    tags$li("Finally, we try to predict a new, unseen Twitter (via user input) account's personality type according 
+    to their Twitter usage."),
+    tags$ul(
+      tags$li("Our prediction method involves selecting the highest occuring category of the new user's 100 closest 
+      neighbors along the variables present in the original dataset that we can reproduce.")
+    )
+  ),
+  
+  p("It is worth noting that in general we find very weak relationships between ", tags$strong("most Twitter usage 
+    statistics")," and personality scores. Further, our prediction technique is only marginally better than random 
+    chance. We conclude that though the field of personality prediction is important and exploding in today's world of 
+    `Big Data`, that:"),
+  tags$ul(
+    tags$li("larger datasets are needed (n=3000 is far too small)"),
+    tags$li("the voluntary response bias inherent in this 
+    dataset makes useful analysis difficult"),
+    tags$li("and finally that Twitter - with 140 character limits and widespread usage archetypes, i.e. that people 
+    tweet in similar ways regardless of personal differences - make personality predictions difficult.")
+  ),
+  
+  p("Further work might yield useful results if the more hetereogenous Twitter usage variables present in the 
+    original dataset could be reproduced, or if a new larger dataset could be created and made accessible."),
+  
+  h3("Initial Exploration"),
+  
+  p("Choose a single personality dimension to explore."),
+  
+  fluidRow(
+    column(3,
+           p("Big5"),
+           checkboxInput("openness", "Openness", value=T),
+           checkboxInput("conscientiousness", "Conscientiousness"),
+           checkboxInput("extraversion", "Extraversion"),
+           checkboxInput("agreeableness", "Agreeableness"),
+           checkboxInput("Neuroticism", "Neuroticism"),
+           hr(),
+           p("Dark Triad"),
+           checkboxInput("Machiavellianism", "Machiavellianism"),
+           checkboxInput("narcissism", "Narcissism"),
+           checkboxInput("psychopathy", "Psychopathy")
+    ),
+    column(9,
+           plotOutput('initialExp')
+    )
+  ),
+  
+  hr(),
+  
+  h3("Relationship Exploration"),
+  
+  p("Choose a personality dimension to explore against a Twitter usage variable. Note for `followers_count`, an extreme outlier (2.9M when mean is 1280) is removed for visualization purposes."),
+  
+  fluidRow(
+    column(4,
+     fluidRow(
+       column(6,
+              p('X'),
+              checkboxInput("openness2", "Openness", value=T),
+              checkboxInput("conscientiousness2", "Conscientiousness"),
+              checkboxInput("extraversion2", "Extraversion"),
+              checkboxInput("agreeableness2", "Agreeableness"),
+              checkboxInput("Neuroticism2", "Neuroticism"),
+              checkboxInput("Machiavellianism2", "Machiavellianism"),
+              checkboxInput("narcissism2", "Narcissism"),
+              checkboxInput("psychopathy2", "Psychopathy")
+              ),
+       column(6,
+              p('Y'),
+              checkboxInput("favourites_count", "Favourites Count", value=T),
+              checkboxInput("followers_count", "Followers Count"),
+              checkboxInput("friends_count", "Friends Count"),
+              checkboxInput("statuses_count", "Statuses Count"),
+              checkboxInput("kloutscore", "Kloutscore"),
+              checkboxInput("PercentOrigTweets", "Percent Original"),
+              checkboxInput("PercentRT", "Percent Retweet"),
+              checkboxInput("PercentReplies", "Percent Replies")
+              )
+      ),
+     hr(),
+     p('Log Y?'),
+     checkboxInput("log", "LogY"),
+     p('geom_smooth?'),
+     checkboxInput("geom_smooth", "Add geom_smooth")
+     ),
+    column(8,
+      plotOutput('relExp')
+    )
+  ),
+  
   hr(),
   
   h3("Cluster Visualization"),
-  
-  p("Visualized below in two dimensions (via PCA) is our dataset of Twitter users (n~=3000) when clustered according to a user-specified k. The subset and k you choose below will determine which subset of the dataset will be used throughout the rest of this Shiny App as well as the number of categories / personality types."),
-  
-  p("(NB: this methodology uses k-means clustering and so the resultant clusters are by definition 'different' from each other which should not be necessarily interpreted as meaning they are meaningful. Nonetheless, we saw useful clusters when k = 5.)"),
-  
+  p("Visualized below in two dimensions (via PCA) is our dataset of Twitter users (n~=3000) when clustered according 
+    to a user-specified k. Big5 will be used as the subset for later prediction regardless of your choice but the k you 
+  choose below will determine the number of categories / personality types."),
+  p("(NB: this methodology uses k-means clustering and so the resultant clusters are by definition 'different' from each other which should not be necessarily interpreted as meaning they are meaningful. Nonetheless, we saw useful clusters when k = 3.)"),
   plotOutput('clusterPlot'),
-  
   fluidRow(
     column(5, offset = 1,
      h4("Choose subset to visualize"),
      checkboxInput('big5', 'Big5', value=T),
      checkboxInput('dark3', 'Dark Triad'),
      checkboxInput('all8', 'Both (All8)')
-     
     ),
     column(5,
      sliderInput("clusters",
@@ -49,25 +146,46 @@ shinyUI(fluidPage(
   hr(),
   
   h3("What Each Cluster Means, WRT Personality Scores"),
-  
   p("The clusters created above are visualized below across your chosen subset's personality dimensions."),
-  
   plotOutput('clusterViz'),
   
   hr(),
   
+  h3("Personality Types (Clusters) and Twitter Behavior"),
+  p("All extreme outliers have been removed. Regardless of the subset you chose earlier, the next few sections
+    work with the Big5 subset."),
+  fluidRow(
+    column(2,
+           p('Y'),
+           checkboxInput("favourites_count2", "Favourites Count", value=T),
+           checkboxInput("followers_count2", "Followers Count"),
+           checkboxInput("friends_count2", "Friends Count"),
+           checkboxInput("statuses_count2", "Statuses Count"),
+           checkboxInput("kloutscore2", "Kloutscore"),
+           checkboxInput("PercentOrigTweets2", "Percent Original"),
+           checkboxInput("PercentRT2", "Percent Retweet"),
+           checkboxInput("PercentReplies2", "Percent Replies")
+    ),
+    column(10,
+          plotOutput('catAndTweets')
+   )
+  ),
+  
+  
+  hr(),
+  
   h3("Twitter Data"),
-  
   p("Choose a user, note that the Twitter API does not provide 'an exhaustive source of Tweets. Not all Tweets will be indexed or made available via the search interface.'"),
-  
   div(style="display: inline-block;vertical-align:right; width: 150px;",textInput("twitterUser1", "Enter User", "realDonaldTrump")),
-  actionButton("showUserTweets", "Show User Tweets", style="color: #000000;background-color: #00aced;margin: 4px;"),
+  actionButton("showUserTweets", "Get User Tweet Stats", style="color: #000000;background-color: #00aced;margin: 4px;"),
   fluidRow(
     dataTableOutput("userTweets")
   ),
   
   hr(),
   
-  h3("Exploring Yours User's Tweets")
+  h3("Predicting A User's Personality Type"),
+  actionButton("getPreds", "Get Prediction", style="color: #000000;background-color: #00aced;margin: 4px;"),
+  dataTableOutput("closestTweets")
   
 ))
